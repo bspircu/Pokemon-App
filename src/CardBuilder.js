b6824e696e61291f5d614cae19a2fb1c8e834e6d
Bryan Spircu <bspircu@gmail.com>
2019-09-11 19:15:51 -0700

    Built index with Pokemon, built card builder

---
+++ src/CardBuilder.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+class Cardbuilder extends React.Component{
+    consutuctor(){
+        super()
+
+    }
+
+
+render () {
+    return(
+        <div className = "cardStyles">
+            <h1 className = "pokemonName">{this.props.name}</h1>
+            <img src= {this.props.image} alt="error" />
+            <p>Categories = {this.props.category}</p>
+            <p>Weight = {this.props.weight}</p>
+            <p>Abilities = {this.props.abilities}</p>
+        </div>
+    )
+}
+}
+export default Cardbulider
