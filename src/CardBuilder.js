0810b98b3b51ebbb0ecaa32e75843d288667225b
Bryan Spircu <bspircu@gmail.com>
2019-09-11 20:26:04 -0700

    added in line style to mouse hover on images with state, added more css.

--- src/CardBuilder.js
+++ src/CardBuilder.js
@@ -2,12 +2,26 @@ import React from "react"
 
 
 class Cardbuilder extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            hovered: false
+        }
+    }
+    handleHover = (isHovered) => {
+        this.setState({ hovered: isHovered})
+    }
+
 render () {
     return(
         <div className = "cardStyles">
-            <h1 className = "pokemonName">{this.props.name}</h1>
-            <img src= {this.props.image} alt="error" />
-            <p>Categories = {this.props.category}</p>
+            <h1 className = "PokemonName">{this.props.name}</h1>
+            <img 
+                onMouseEnter = {() => this.handleHover(true)}
+                onMouseLeave = {() => this.handleHover(false)}
+                style = {{border: this.state.hovered ? "ridge" : null}}
+                src= {this.props.image} alt="error" />
+            <p>Category = {this.props.category}</p>
             <p>Weight = {this.props.weight}</p>
             <p>Abilities = {this.props.abilities}</p>
         </div>
